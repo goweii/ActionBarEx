dac7103f4164c3af7ca42b64a09c7a14d60994b5
goweii <goweii@163.com>
2019-11-16 14:16:40 +0800

    移除无用资源

--- actionbarex/src/main/java/per/goweii/actionbarex/ActionBarEx.java
+++ actionbarex/src/main/java/per/goweii/actionbarex/ActionBarEx.java
@@ -3,9 +3,9 @@ package per.goweii.actionbarex;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.SparseArray;
@@ -43,21 +43,20 @@ public class ActionBarEx extends FrameLayout {
 
     private Activity mActivity = null;
 
-    private int mImmersion;
-    private int mBackgroundLayerLayoutRes;
-    private int mBackgroundLayerImageRes;
-    private boolean mStatusBarVisible;
-    private int mStatusBarMode;
-    private int mStatusBarHeight;
-    private int mStatusBarColor;
-    private int mTitleBarLayoutRes;
-    private int mTitleBarHeight;
-    private int mBottomLineColor;
-    private int mBottomLineResId;
-    private int mBottomLineHeight;
-    private int mForegroundLayerLayoutRes;
-    private int mClickToFinishViewId;
-    private boolean mBottomLineOutside;
+    private int mImmersion = IMMERSION_UNCHANGED;
+    private int mBackgroundLayerLayoutRes = 0;
+    private int mBackgroundLayerImageRes = 0;
+    private boolean mStatusBarVisible = false;
+    private int mStatusBarMode = STATUS_BAR_MODE_UNCHANGED;
+    private int mStatusBarColor = Color.TRANSPARENT;
+    private int mTitleBarLayoutRes = 0;
+    private int mTitleBarHeight = LayoutParams.WRAP_CONTENT;
+    private int mBottomLineColor = Color.TRANSPARENT;
+    private int mBottomLineResId = 0;
+    private int mBottomLineHeight = 0;
+    private int mForegroundLayerLayoutRes = 0;
+    private int mClickToFinishViewId = 0;
+    private boolean mBottomLineOutside = false;
 
     private View mBackgroundLayer;
     private LinearLayout mActionBar;
@@ -80,10 +79,26 @@ public class ActionBarEx extends FrameLayout {
     public ActionBarEx(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         hintSystemActionBar();
-        mStatusBarHeight = StatusBarCompat.getHeight(context);
         initAttrs(attrs);
         initView();
         refresh();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mBottomLineOutside) {
+            ViewParent parent = getParent();
+            if (parent instanceof ViewGroup) {
+                ViewGroup viewGroup = (ViewGroup) parent;
+                viewGroup.setClipChildren(false);
+            }
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     public View getBackgroundLayer() {
@@ -141,7 +156,7 @@ public class ActionBarEx extends FrameLayout {
     }
 
     public int getStatusBarHeight() {
-        return mStatusBarVisible ? mStatusBarHeight : 0;
+        return mStatusBarVisible ? StatusBarCompat.getHeight(getContext()) : 0;
     }
 
     public int getTitleBarHeight() {
@@ -150,139 +165,6 @@ public class ActionBarEx extends FrameLayout {
 
     public int getBottomHeight() {
         return mBottomLineHeight;
-    }
-
-    /**
-     * 初始化布局文件传入的配置
-     * 子类可重写并初始化自己定义的属性配置，必须保证在第一行调用super方法
-     *
-     * @param attrs AttributeSet
-     */
-    protected void initAttrs(AttributeSet attrs) {
-        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ActionBarEx);
-
-        float titleBarHeightDef = getContext().getResources().getDimension(R.dimen.actionbarex_common_title_bar_height_def);
-        float bottomLineHeightDef = getContext().getResources().getDimension(R.dimen.actionbarex_common_bottom_line_height_def);
-        int bottomLineColorDef = ContextCompat.getColor(getContext(), R.color.actionbarex_common_bottom_line_color_def);
-        int statusBarColorDef = ContextCompat.getColor(getContext(), R.color.actionbarex_common_status_bar_color_def);
-
-        mImmersion = typedArray.getInt(R.styleable.ActionBarEx_ab_immersion, IMMERSION_UNCHANGED);
-        mBackgroundLayerLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_backgroundLayerLayout, 0);
-        mBackgroundLayerImageRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_backgroundLayerImageRes, 0);
-        mStatusBarVisible = typedArray.getBoolean(R.styleable.ActionBarEx_ab_statusBarVisible, true);
-        mStatusBarMode = typedArray.getInt(R.styleable.ActionBarEx_ab_statusBarMode, STATUS_BAR_MODE_UNCHANGED);
-        mStatusBarColor = typedArray.getColor(R.styleable.ActionBarEx_ab_statusBarColor, statusBarColorDef);
-        mTitleBarLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_titleBarLayout, 0);
-        mTitleBarHeight = (int) typedArray.getDimension(R.styleable.ActionBarEx_ab_titleBarHeight, titleBarHeightDef);
-        mBottomLineHeight = (int) typedArray.getDimension(R.styleable.ActionBarEx_ab_bottomLineHeight, bottomLineHeightDef);
-        mBottomLineColor = typedArray.getColor(R.styleable.ActionBarEx_ab_bottomLineColor, bottomLineColorDef);
-        mBottomLineResId = typedArray.getResourceId(R.styleable.ActionBarEx_ab_bottomLineResId, 0);
-        mBottomLineOutside = typedArray.getBoolean(R.styleable.ActionBarEx_ab_bottomLineOutside, false);
-        mForegroundLayerLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_foregroundLayerLayout, 0);
-        mClickToFinishViewId = typedArray.getResourceId(R.styleable.ActionBarEx_ab_clickToFinish, 0);
-
-        typedArray.recycle();
-    }
-
-    /**
-     * 初始化子TitleBar
-     *
-     * @return TitleBarChild
-     */
-    protected View inflateTitleBar() {
-        if (mTitleBarLayoutRes > 0) {
-            return inflate(getContext(), mTitleBarLayoutRes, null);
-        }
-        return null;
-    }
-
-    private void initView() {
-        // 1 初始化BackgroundLayer
-        if (mBackgroundLayerLayoutRes > 0) {
-            mBackgroundLayer = inflate(getContext(), mBackgroundLayerLayoutRes, null);
-            addViewInLayout(mBackgroundLayer, getChildCount(), makeLayerLayoutParams(), true);
-        } else {
-            if (mBackgroundLayerImageRes > 0) {
-                ImageView actionBarImageView = new ImageView(getContext());
-                actionBarImageView.setImageResource(mBackgroundLayerImageRes);
-                actionBarImageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                addViewInLayout(actionBarImageView, getChildCount(), makeLayerLayoutParams(), true);
-            }
-        }
-
-        // 2 初始ActionBarLayer
-        mActionBar = (LinearLayout) inflate(getContext(), R.layout.actionbarex_action_bar, null);
-        mActionBar.setLayoutParams(makeLayoutParamsWithHeight(getActionBarHeight()));
-        // 2.1 初始StatusBar
-        mStatusBar = mActionBar.findViewById(R.id.actionbarex_status_bar);
-        mStatusBar.setLayoutParams(makeLayoutParamsWithHeight(mStatusBarHeight));
-        mStatusBar.setVisibility(mStatusBarVisible ? VISIBLE : GONE);
-        // 2.2 初始TitleBar
-        mTitleBar = mActionBar.findViewById(R.id.actionbarex_title_bar);
-        mTitleBar.setClickable(true);
-        mTitleBar.setFocusable(true);
-        mTitleBar.setFocusableInTouchMode(true);
-        mTitleBar.setLayoutParams(makeLayoutParamsWithHeight(mTitleBarHeight));
-        mTitleBarChild = inflateTitleBar();
-        if (mTitleBarChild != null) {
-            mTitleBar.addView(mTitleBarChild);
-        }
-        // 2.3 初始BottomLine
-        mBottomLine = mActionBar.findViewById(R.id.actionbarex_bottom_line);
-        mBottomLine.setLayoutParams(makeLayoutParamsWithHeight(mBottomLineHeight));
-        if (mBottomLineResId > 0) {
-            mBottomLine.setBackgroundResource(mBottomLineResId);
-        } else {
-            mBottomLine.setBackgroundColor(mBottomLineColor);
-        }
-        if (mBottomLineOutside) {
-            mActionBar.setClipChildren(false);
-            setClipChildren(false);
-        }
-        addViewInLayout(mActionBar, getChildCount(), makeLayoutParamsWithHeight(getActionBarHeight()), true);
-
-        // 3 初始ForegroundLayer
-        if (mForegroundLayerLayoutRes > 0) {
-            mForegroundLayer = inflate(getContext(), mForegroundLayerLayoutRes, null);
-            addViewInLayout(mForegroundLayer, getChildCount(), makeLayerLayoutParams(), true);
-        }
-        performClickToFinish();
-    }
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        if (mBottomLineOutside) {
-            ViewParent parent = getParent();
-            if (parent instanceof ViewGroup) {
-                ViewGroup viewGroup = (ViewGroup) parent;
-                viewGroup.setClipChildren(false);
-            }
-        }
-    }
-
-    private void performClickToFinish() {
-        if (mTitleBarChild == null) {
-            return;
-        }
-        View clickToFinishView = getView(mClickToFinishViewId);
-        if (clickToFinishView == null) {
-            return;
-        }
-        clickToFinishView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finishActivity();
-            }
-        });
-    }
-
-    private LayoutParams makeLayerLayoutParams() {
-        return new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getActionBarHeight());
-    }
-
-    private LinearLayout.LayoutParams makeLayoutParamsWithHeight(int height) {
-        return new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
     }
 
     /**
@@ -351,6 +233,118 @@ public class ActionBarEx extends FrameLayout {
     }
 
     /**
+     * 初始化布局文件传入的配置
+     * 子类可重写并初始化自己定义的属性配置，必须保证在第一行调用super方法
+     *
+     * @param attrs AttributeSet
+     */
+    protected void initAttrs(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ActionBarEx);
+        mImmersion = typedArray.getInt(R.styleable.ActionBarEx_ab_immersion, mImmersion);
+        mBackgroundLayerLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_backgroundLayerLayout, mBackgroundLayerLayoutRes);
+        mBackgroundLayerImageRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_backgroundLayerImageRes, mBackgroundLayerLayoutRes);
+        mStatusBarVisible = typedArray.getBoolean(R.styleable.ActionBarEx_ab_statusBarVisible, mStatusBarVisible);
+        mStatusBarMode = typedArray.getInt(R.styleable.ActionBarEx_ab_statusBarMode, mStatusBarMode);
+        mStatusBarColor = typedArray.getColor(R.styleable.ActionBarEx_ab_statusBarColor, mStatusBarColor);
+        mTitleBarLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_titleBarLayout, mTitleBarLayoutRes);
+        mTitleBarHeight = (int) typedArray.getDimension(R.styleable.ActionBarEx_ab_titleBarHeight, mTitleBarHeight);
+        mBottomLineHeight = (int) typedArray.getDimension(R.styleable.ActionBarEx_ab_bottomLineHeight, mBottomLineHeight);
+        mBottomLineColor = typedArray.getColor(R.styleable.ActionBarEx_ab_bottomLineColor, mBottomLineColor);
+        mBottomLineResId = typedArray.getResourceId(R.styleable.ActionBarEx_ab_bottomLineResId, mBottomLineResId);
+        mBottomLineOutside = typedArray.getBoolean(R.styleable.ActionBarEx_ab_bottomLineOutside, mBottomLineOutside);
+        mForegroundLayerLayoutRes = typedArray.getResourceId(R.styleable.ActionBarEx_ab_foregroundLayerLayout, mForegroundLayerLayoutRes);
+        mClickToFinishViewId = typedArray.getResourceId(R.styleable.ActionBarEx_ab_clickToFinish, mClickToFinishViewId);
+        typedArray.recycle();
+    }
+
+    /**
+     * 初始化子TitleBar
+     *
+     * @return TitleBarChild
+     */
+    protected View inflateTitleBar() {
+        if (mTitleBarLayoutRes > 0) {
+            return inflate(getContext(), mTitleBarLayoutRes, null);
+        }
+        return null;
+    }
+
+    private void initView() {
+        // 1 初始化BackgroundLayer
+        if (mBackgroundLayerLayoutRes > 0) {
+            mBackgroundLayer = inflate(getContext(), mBackgroundLayerLayoutRes, null);
+            addViewInLayout(mBackgroundLayer, getChildCount(), makeLayerLayoutParams(), true);
+        } else {
+            if (mBackgroundLayerImageRes > 0) {
+                ImageView actionBarImageView = new ImageView(getContext());
+                actionBarImageView.setImageResource(mBackgroundLayerImageRes);
+                actionBarImageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                addViewInLayout(actionBarImageView, getChildCount(), makeLayerLayoutParams(), true);
+            }
+        }
+        // 2 初始ActionBarLayer
+        mActionBar = (LinearLayout) inflate(getContext(), R.layout.actionbarex_action_bar, null);
+        mActionBar.setLayoutParams(makeLayoutParamsWithHeight(getActionBarHeight()));
+        // 2.1 初始StatusBar
+        mStatusBar = mActionBar.findViewById(R.id.actionbarex_status_bar);
+        mStatusBar.setLayoutParams(makeLayoutParamsWithHeight(StatusBarCompat.getHeight(getContext())));
+        mStatusBar.setVisibility(mStatusBarVisible ? VISIBLE : GONE);
+        // 2.2 初始TitleBar
+        mTitleBar = mActionBar.findViewById(R.id.actionbarex_title_bar);
+        mTitleBar.setClickable(true);
+        mTitleBar.setFocusable(true);
+        mTitleBar.setFocusableInTouchMode(true);
+        mTitleBar.setLayoutParams(makeLayoutParamsWithHeight(mTitleBarHeight));
+        mTitleBarChild = inflateTitleBar();
+        if (mTitleBarChild != null) {
+            mTitleBar.addView(mTitleBarChild);
+        }
+        // 2.3 初始BottomLine
+        mBottomLine = mActionBar.findViewById(R.id.actionbarex_bottom_line);
+        mBottomLine.setLayoutParams(makeLayoutParamsWithHeight(mBottomLineHeight));
+        if (mBottomLineResId > 0) {
+            mBottomLine.setBackgroundResource(mBottomLineResId);
+        } else {
+            mBottomLine.setBackgroundColor(mBottomLineColor);
+        }
+        if (mBottomLineOutside) {
+            mActionBar.setClipChildren(false);
+            setClipChildren(false);
+        }
+        addViewInLayout(mActionBar, getChildCount(), makeLayoutParamsWithHeight(getActionBarHeight()), true);
+        // 3 初始ForegroundLayer
+        if (mForegroundLayerLayoutRes > 0) {
+            mForegroundLayer = inflate(getContext(), mForegroundLayerLayoutRes, null);
+            addViewInLayout(mForegroundLayer, getChildCount(), makeLayerLayoutParams(), true);
+        }
+        performClickToFinish();
+    }
+
+    private void performClickToFinish() {
+        if (mTitleBarChild == null) {
+            return;
+        }
+        View clickToFinishView = getView(mClickToFinishViewId);
+        if (clickToFinishView == null) {
+            return;
+        }
+        clickToFinishView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishActivity();
+            }
+        });
+    }
+
+    private LayoutParams makeLayerLayoutParams() {
+        return new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getActionBarHeight());
+    }
+
+    private LinearLayout.LayoutParams makeLayoutParamsWithHeight(int height) {
+        return new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
+    }
+
+    /**
      * 隐藏默认的ActionBar
      */
     private void hintSystemActionBar() {
@@ -383,15 +377,5 @@ public class ActionBarEx extends FrameLayout {
 
     public void finishActivity() {
         Utils.finishActivity(getContext());
-    }
-
-    @Override
-    public boolean isInEditMode() {
-        return false;
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
